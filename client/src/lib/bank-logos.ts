b869727b94052c8c7391900e231c88996738252d
heboah5-tech <heboah5@gmail.com>
2026-05-17 15:31:24 +0300

    Update bank-logos.ts

--- client/src/lib/bank-logos.ts
+++ client/src/lib/bank-logos.ts
@@ -41,36 +41,6 @@ export const BANK_LOGOS: BankLogo[] = [
     logo: "https://vid.alarabiya.net/images/2018/10/22/dbcecf41-6800-4815-a624-a5e34593ace3/dbcecf41-6800-4815-a624-a5e34593ace3_16x9_1200x676.png",
     label: "بنك الخليج الدولي",
   },
-  {
-    match: /\bbsf\b|saudi fransi|banque fransi|fransi|الفرنسي/i,
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/BSF_logo.svg/1280px-BSF_logo.svg.png",
-    label: "البنك السعودي الفرنسي",
-  },
-  {
-    match: /\banb\b|arab national|العربي الوطني|البنك العربي/i,
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Arab_National_Bank_logo.svg/1280px-Arab_National_Bank_logo.svg.png",
-    label: "البنك العربي الوطني",
-  },
-  {
-    match: /samba|سامبا/i,
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Samba_Financial_Group_logo.svg/1280px-Samba_Financial_Group_logo.svg.png",
-    label: "مجموعة سامبا المالية",
-  },
-  {
-    match: /\bstc\s*pay\b|stcpay|اس تي سي|stc بنك/i,
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Stc_pay_logo.svg/1280px-Stc_pay_logo.svg.png",
-    label: "STC Pay",
-  },
-  {
-    match: /emirates\s*nbd|الإمارات\s*دبي|الامارات\s*دبي|\bnbd\b/i,
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Emirates_NBD_Logo.svg/1280px-Emirates_NBD_Logo.svg.png",
-    label: "بنك الإمارات دبي الوطني",
-  },
-  {
-    match: /\bd360\b|د360/i,
-    logo: "https://d360.com/wp-content/uploads/2024/02/d360-logo.svg",
-    label: "بنك D360",
-  },
 ];
 
 export function findBankLogo(bankName?: string | null): BankLogo | null {
